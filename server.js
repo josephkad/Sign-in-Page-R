dcba5fa064e308a7911cb6f9f9f7eb68acc79a7e
JosephK-creator <jnxzue@gmail.com>
2026-06-05 14:15:16 -0400

    transporter improvements

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
